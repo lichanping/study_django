4e3d4005e051fa5de6ff44735815c256b24c5f6a
shirley.licp <shirley.licp@ef.com>
2017-07-06 17:50:05 +0800

    finish all lessons.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,35 @@ from django.http import HttpResponse
 
 from . import models
 
+
 def index(request):
-    article_val = models.Article.objects.get(pk=1)
-    return render(request, 'index.html', {'article': article_val})
+    articles = models.Article.objects.all()
+    return render(request, 'index.html', {'articles': articles})
+
+
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'article_page.html', {'article': article})
+
+
+def edit_page(request, article_id):
+    if str(article_id) == "0":
+        return render(request, 'edit_page.html')
+    else:
+        article = models.Article.objects.get(pk=article_id)
+        return render(request, 'edit_page.html', {'article': article})
+
+
+def edit_action(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    article_id = request.POST.get('article_id', '0')
+    if article_id == '0':
+        models.Article.objects.create(title=title, content=content)
+    else:
+        article = models.Article.objects.get(pk=article_id)
+        article.title = title
+        article.content = content
+        article.save()
+    articles = models.Article.objects.all()
+    return render(request, 'index.html', {'articles': articles})
